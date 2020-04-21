f630c4c742ea5c33a3ca2430a4c7fafc44f7dab6
Raphael <raphael.muller@ens.uvsq.fr>
2020-04-21 11:04:12 +0200

    ajouts classes article

--- src/main/java/model/Article.java
+++ src/main/java/model/Article.java
@@ -1,5 +1,11 @@
 package model;
 
 public class Article {
-
+	
+	protected Integer id;
+	protected Integer nbDisque;
+	//protected Adherent emprinteur; TODO décommenter
+	//protected Film film;
+	
+	
 }
