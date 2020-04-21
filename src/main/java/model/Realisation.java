e5950a66c59da937c5cdce4400399634b4a1a088
Thomas347 <thomas.guinebert15@gmail.com>
2020-04-21 12:42:21 +0200

    realisation et realisationKey

--- src/main/java/model/Realisation.java
+++ src/main/java/model/Realisation.java
@@ -2,4 +2,52 @@ package model;
 
 public class Realisation {
 
+	private RealisationKey id;
+
+	
+	public Realisation() {
+	}
+
+	public Realisation(RealisationKey id) {
+		this.id = id;
+	}
+
+	public RealisationKey getId() {
+		return id;
+	}
+
+	public void setId(RealisationKey id) {
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Realisation other = (Realisation) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+	
+	
 }
